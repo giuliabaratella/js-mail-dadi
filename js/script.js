8f1f4ea5b0f19911ed7d8a649dbb78233238c961
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-12 15:36:21 +0200

    added email list + button and email check variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const validEmails = [
+    'giuliab@boolean.com',
+    'giuliab@gmail.com',
+    'giuliab@yahoo.com'
+]
+const btn = document.querySelector('button');
+const emailCheck = document.getElementById('email').value ;
+console.log (validEmails, btn);
+
+
